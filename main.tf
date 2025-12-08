5a01de804bd37ea6ff36629aad6f467e38a5c784
Nicholas Cruz <ncruz@ntracts.com>
2025-12-08 13:45:59 -0500

    Fix env variable references and format sku value

--- main.tf
+++ main.tf
@@ -8,16 +8,17 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "project-plato-rg" {
-  name     = "${var.project}-${environment}"
+  name     = "${var.project}-${var.environment}"
   location = var.location
 }
 
 resource "azurerm_container_registry" "project-plato-container-regsitry" {
-  name                = "${var.project}-${environment}-registry"
+  name                = "${var.project}${var.environment}registry"
   resource_group_name = azurerm_resource_group.project-plato-rg.name
   location            = var.location
-  sku                 = Basic
+  sku                 = "Basic"
 }
